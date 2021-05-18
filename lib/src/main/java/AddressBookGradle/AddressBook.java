19456218c6ac0bf8e6d9565f16e7787c254318e1
Shruti Verma <shrutiverma3105@gmail.com>
2021-05-18 12:54:37 +0530

    commit UC2

--- lib/src/main/java/AddressBookGradle/AddressBook.java
+++ lib/src/main/java/AddressBookGradle/AddressBook.java
@@ -1,9 +1,60 @@
 package AddressBookGradle;
 
+import java.util.Scanner;
+
 public class AddressBook {
 	public static void main(String[] args) {
-        Contact contactPerson = new Contact("Shruti", "Verma", "Awas Vikas", "Kanpur", "Uttar Pradesh",
-                208017, 12345, "shrutiverma3105@gmail.com");
-        System.out.println(contactPerson.toString());
+        Contact contact = new Contact();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Address Book Menu");
+        boolean isAlive = true;
+        while (isAlive) {
+            System.out.println("Enter choice: ");
+            System.out.println("1. For Add a new Contact to AddressBook.");
+            System.out.println("2. For Display Contact Detial.");
+            System.out.println("3. For Exit.");
+            System.out.print("Enter choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+
+            switch (choice) {
+            case 1:
+                System.out.println("Enter the first_name:-");
+                contact.setFirst_name(sc.nextLine());
+                System.out.println("Enter the last_name:-");
+                contact.setLast_name(sc.nextLine());
+                System.out.println("Enter the address_name:-");
+                contact.setAddress(sc.nextLine());
+                System.out.println("Enter the city_name:-");
+                contact.setCity(sc.nextLine());
+                System.out.println("Enter the state_name:-");
+                contact.setState(sc.nextLine());
+                System.out.println("Enter the zip_number:-");
+                contact.setZip(sc.nextInt());
+                System.out.println("Enter the phone_number:-");
+                contact.setPhone_number(sc.nextLong());
+                sc.nextLine();
+                System.out.println("Enter the email:-");
+                contact.setEmail(sc.nextLine());
+                isAlive = true;
+                break;
+
+            case 2:
+                System.out.println(contact.toString());
+                System.out.println("Contact is added successfully.");
+                break;
+
+            case 3:
+                isAlive = false;
+                System.out.println("Exit.....");
+                break;
+
+            default:
+                System.out.println("You have entered wrong choice.");
+                isAlive = true;
+                break;
+            }
+        }
+        sc.close();
     }
 }
--- lib/src/main/java/AddressBookGradle/Contact.java
+++ lib/src/main/java/AddressBookGradle/Contact.java
@@ -25,6 +25,72 @@ public class Contact {
     Contact() {
         this("", "", "", "", "", 0, 0, "");
     }
+    
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setZip(int zip) {
+        this.zip = zip;
+    }
+
+    public void setPhone_number(long phone_number) {
+        this.phone_number = phone_number;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter Method
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public int getZip() {
+        return zip;
+    }
+
+    public long getPhone_number() {
+        return phone_number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
     public String toString() {
         return "Person Details: \t \nFirst name:" + this.first_name + " \t \nLast name:" + this.last_name
