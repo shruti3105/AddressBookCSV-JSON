4255b92fa50295e5931482a1c29aea2130be9dce
shruti3105 <82582531+shruti3105@users.noreply.github.com>
2021-05-22 14:04:27 +0530

    Update AddressBook.java

--- lib/src/main/java/AddressBookGradle/AddressBook.java
+++ lib/src/main/java/AddressBookGradle/AddressBook.java
@@ -3,58 +3,45 @@ package AddressBookGradle;
 import java.util.Scanner;
 
 public class AddressBook {
-	public static void main(String[] args) {
-        Contact contact = new Contact();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the Address Book Menu");
-        boolean isAlive = true;
-        while (isAlive) {
-            System.out.println("Enter choice: ");
-            System.out.println("1. For Add a new Contact to AddressBook.");
-            System.out.println("2. For Display Contact Detial.");
-            System.out.println("3. For Exit.");
-            System.out.print("Enter choice: ");
-            int choice = sc.nextInt();
-            sc.nextLine();
-
-            switch (choice) {
-            case 1:
-                System.out.println("Enter the first_name:-");
-                contact.setFirst_name(sc.nextLine());
-                System.out.println("Enter the last_name:-");
-                contact.setLast_name(sc.nextLine());
-                System.out.println("Enter the address_name:-");
-                contact.setAddress(sc.nextLine());
-                System.out.println("Enter the city_name:-");
-                contact.setCity(sc.nextLine());
-                System.out.println("Enter the state_name:-");
-                contact.setState(sc.nextLine());
-                System.out.println("Enter the zip_number:-");
-                contact.setZip(sc.nextInt());
-                System.out.println("Enter the phone_number:-");
-                contact.setPhone_number(sc.nextLong());
-                sc.nextLine();
-                System.out.println("Enter the email:-");
-                contact.setEmail(sc.nextLine());
-                isAlive = true;
-                break;
-
-            case 2:
-                System.out.println(contact.toString());
-                System.out.println("Contact is added successfully.");
-                break;
-
-            case 3:
-                isAlive = false;
-                System.out.println("Exit.....");
-                break;
-
-            default:
-                System.out.println("You have entered wrong choice.");
-                isAlive = true;
-                break;
-            }
-        }
-        sc.close();
+	public static void main(String args[]) {
+		AddressBook ad = new AddressBook();
+		Scanner sc = new Scanner(System.in);
+		String m;
+        System.out.println("Enter your first name:");
+	    String firstName = sc.nextLine();
+	    ad.setFirstName(firstName);
+	    System.out.println("Enter the last name :");
+	    String lastName = sc.nextLine();
+	    ad.setLastName(lastName);
+	    System.out.println("Enter the address :");
+	    String address = sc.nextLine();
+	    ad.setAddress(address);
+	    System.out.println("Enter the city :");
+	    String city = sc.nextLine();
+	    ad.setCity(city);
+	    System.out.println("Enter the state :");
+	    String state = sc.nextLine();
+	    ad.setState(state);
+	    System.out.println("Enter the email :");
+	    String email = sc.nextLine();
+	    ad.setEmail(email);
+	    System.out.println("Enter the mobile number :");
+	    String phoneNo = sc.next();
+	    ad.setPhoneNo(phoneNo);
+	    System.out.println("Enter the zip :");
+	    int zip = sc.nextInt();
+	    ad.setZip(zip);
+	    m = "First Name :- "+ad.getFirstName()+
+	            	"Last name : - "+ad.getLastName()+
+	            	"Address :- "+ad.getAddress()+
+	            	"City :- "+ad.getCity()+
+	            	"State :- "+ad.getCity()+
+	            	"Email:- "+ad.getEmail()+
+	            	"MobileNo. :- "+ad.getPhoneNo()+
+	            	"Zip :- "+ad.getZip();
+	    
+	    System.out.println(m);
     }
+		
+	
 }
